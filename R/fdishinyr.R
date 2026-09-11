2699b634d5d01e5db34741244b6d4def7b764ed3
eblondel <emmanuel.blondel1@gmail.com>
2026-09-11 14:10:06 +0200

    fix imports

--- R/fdishinyr.R
+++ R/fdishinyr.R
@@ -1,12 +1,14 @@
 #' FDI shiny utils
 #'
-#' FDI shiny utils
+#' fdishinyr provides common widgets for use with shiny.
 #' 
-#' @import shiny
+#' @rawNamespace import(shiny, except = c(tabsetPanel,column))
 #' @import shiny.i18n
-#' @import bs4Dash
+#' @importFrom shinycssloaders withSpinner
+#' @importFrom bs4Dash dashboardPage dashboardHeader dashboardBrand dashboardSidebar dashboardControlbar dashboardBody tabsetPanel box bs4InfoBox valueBox column
 #' @import dplyr
 #' @import rlang
+#' @import plotly
 #' 
 #' @name fdishinyr
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
